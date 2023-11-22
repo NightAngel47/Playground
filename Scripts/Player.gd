cb6d8d9ade0df8c68b771af82b71b109a7ffbe1c
Steven Drovie <NightAngel47@users.noreply.github.com>
2023-11-21 22:48:18 -0600

    Level with Lights

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,15 +1,11 @@
 class_name Player
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -700.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _ready():
-	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,11 +17,17 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction >= 0
+		$Camera2D.drag_horizontal_offset = direction
+		$AnimatedSprite2D.play()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.pause()
 
 	move_and_slide()
+	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free() #destroy player when off screen, probably want something better
